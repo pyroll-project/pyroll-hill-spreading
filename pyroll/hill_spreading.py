bfe4ac764dcd98efcef2dd0d2efe7cf9f30ca08d
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2023-01-17 14:16:37 +0100

    added hatch and updated

--- pyroll/hill_spreading.py
+++ pyroll/hill_spreading.py
@@ -1,14 +1,17 @@
 import numpy as np
-from pyroll.core import RollPass
+
+from pyroll.core import RollPass, root_hooks, Unit
 from pyroll.core.hooks import Hook
+
+VERSION = "2.0.0b"
 
 RollPass.hill_exponent = Hook[float]()
 
 
 @RollPass.hill_exponent
 def hill_exponent(self: RollPass):
-    equivalent_height_change = self.in_profile.equivalent_rectangle.height - self.out_profile.equivalent_rectangle.height
-    in_equivalent_width = self.in_profile.equivalent_rectangle.width
+    equivalent_height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
+    in_equivalent_width = self.in_profile.equivalent_width
 
     return 0.5 * np.exp(- in_equivalent_width / (2 * np.sqrt(self.roll.working_radius * equivalent_height_change)))
 
@@ -17,14 +20,14 @@ def hill_exponent(self: RollPass):
 def width(self: RollPass.OutProfile):
     roll_pass = self.roll_pass()
 
+    if not self.has_set_or_cached("width"):
+        self.width = roll_pass.roll.groove.usable_width
+
     equivalent_compression = (
             roll_pass.in_profile.equivalent_rectangle.height / roll_pass.out_profile.equivalent_rectangle.height)
     spread = equivalent_compression ** roll_pass.hill_exponent
 
-    if not self.has_set_or_cached("width"):
-        self.width = roll_pass.roll.groove.usable_width
-
     return spread * roll_pass.in_profile.width
 
 
-RollPass.root_hooks.add(RollPass.OutProfile.width)
+root_hooks.add(Unit.OutProfile.width)
