d75b9607d0bb6ac71951baa2b7fb044ef39b358b
Christoph Renzing <64863686+ChRen95@users.noreply.github.com>
2024-09-19 08:53:28 +0200

    updated according to issues with pillar model and introduced Hill prefactor as a hook. (#3)

--- pyroll/hill_spreading.py
+++ pyroll/hill_spreading.py
@@ -1,23 +1,31 @@
-import importlib.util
+import logging
 
 import numpy as np
 
 from pyroll.core import RollPass, ThreeRollPass, root_hooks, Unit
 from pyroll.core.hooks import Hook
 
-VERSION = "2.0.0"
-PILLAR_MODEL_INSTALLED = bool(importlib.util.find_spec("pyroll.pillar_model"))
+VERSION = "2.0.1"
+PILLAR_MODEL_LOADED = False
 
 RollPass.hill_exponent = Hook[float]()
 """Exponent w for for Hill's spread equation."""
 
-root_hooks.add(Unit.OutProfile.width)
+RollPass.hill_pre_factor = Hook[float]()
+"""Pre factor k for Hill's spread equation.'"""
+
+
+@RollPass.hill_pre_factor
+def default_hill_pre_factor(self: RollPass):
+    return 0.5
 
 
 @RollPass.hill_exponent
 def hill_exponent(self: RollPass):
     height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
-    return 0.5 * np.exp(- self.in_profile.equivalent_width / (2 * np.sqrt(self.roll.working_radius * height_change)))
+
+    return self.hill_pre_factor * np.exp(
+        - self.in_profile.equivalent_width / (2 * np.sqrt(self.roll.working_radius * height_change)))
 
 
 @RollPass.OutProfile.width
@@ -34,8 +42,9 @@ def width(self: RollPass.OutProfile):
 def width(self: RollPass.OutProfile):
     rp = self.roll_pass
 
-    if not self.has_set_or_cached("width"):
-        return None
+    if not (PILLAR_MODEL_LOADED and rp.disk_elements):
+        if not self.has_set_or_cached("width"):
+            return None
 
     return rp.draught ** -rp.hill_exponent * rp.in_profile.width
 
@@ -45,5 +54,8 @@ try:
     def pillar_spreads(self: RollPass.DiskElement):
         rp = self.roll_pass
         return self.pillar_draughts ** -rp.hill_exponent
+
+    PILLAR_MODEL_LOADED = True
+
 except AttributeError:
-    pass  # pillar model not loaded
+    logging.getLogger(__name__).debug("Pillar model not loaded. Can not register respective hook function.")
