a5795342406e6617c9c6c8ad6b5cfd201c8ef64f
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-05 14:37:01 +0100

    bump. vers.

--- pyroll/hill_spreading.py
+++ pyroll/hill_spreading.py
@@ -4,7 +4,7 @@ import numpy as np
 from pyroll.core import BaseRollPass, RollPass, ThreeRollPass, root_hooks, Unit
 from pyroll.core.hooks import Hook
 
-VERSION = "2.0.1"
+VERSION = "2.0.2"
 PILLAR_MODEL_LOADED = False
 
 BaseRollPass.hill_exponent = Hook[float]()
