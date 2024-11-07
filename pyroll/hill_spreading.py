ace513197c40bd46d5a02b5b241d7ae8d94a3b61
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-07 11:18:17 +0100

    fix

--- pyroll/hill_spreading.py
+++ pyroll/hill_spreading.py
@@ -1,7 +1,7 @@
 import logging
 import numpy as np
 
-from pyroll.core import BaseRollPass, RollPass, ThreeRollPass, root_hooks, Unit
+from pyroll.core import SymmetricRollPass, RollPass, ThreeRollPass, root_hooks, Unit
 from pyroll.core.hooks import Hook
 
 VERSION = "3.0.0"
@@ -14,36 +14,25 @@ BaseRollPass.hill_pre_factor = Hook[float]()
 """Pre factor k for Hill's spread equation.'"""
 
 
-@BaseRollPass.hill_pre_factor
-def default_hill_pre_factor(self: BaseRollPass):
+@SymmetricRollPass.hill_pre_factor
+def default_hill_pre_factor(self: SymmetricRollPass):
     return 0.5
 
 
-@BaseRollPass.hill_exponent
-def hill_exponent(self: BaseRollPass):
+@SymmetricRollPass.hill_exponent
+def hill_exponent(self: SymmetricRollPass):
     height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
 
     return self.hill_pre_factor * np.exp(
         - self.in_profile.equivalent_width / (2 * np.sqrt(self.roll.working_radius * height_change)))
 
 
-@RollPass.OutProfile.width
-def width(self: RollPass.OutProfile):
+@SymmetricRollPass.OutProfile.width
+def width(self: SymmetricRollPass.OutProfile):
     rp = self.roll_pass
 
     if not self.has_set_or_cached("width"):
         return None
-
-    return rp.draught ** -rp.hill_exponent * rp.in_profile.width
-
-
-@ThreeRollPass.OutProfile.width
-def width(self: RollPass.OutProfile):
-    rp = self.roll_pass
-
-    if not (PILLAR_MODEL_LOADED and rp.disk_elements):
-        if not self.has_set_or_cached("width"):
-            return None
 
     return rp.draught ** -rp.hill_exponent * rp.in_profile.width
 
